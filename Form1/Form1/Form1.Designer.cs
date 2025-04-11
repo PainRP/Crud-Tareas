f1df8617531e359e5573e19b2e4b7468193a52e4
PainRP <jpereza62@miumg.edu.gt>
2025-04-11 02:07:17 -0600

    Proyecto CRUD

---
+++ Form1/Form1/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace Form1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonSaludar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxCarnet = new System.Windows.Forms.TextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxSeccion = new System.Windows.Forms.TextBox();
+            this.textBoxEstudiante = new System.Windows.Forms.TextBox();
+            this.buttonCrear = new System.Windows.Forms.Button();
+            this.buttonBuscar = new System.Windows.Forms.Button();
+            this.buttonActualizar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonTareas = new System.Windows.Forms.Button();
+            this.comboBoxSeccion = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonSaludar
+            // 
+            this.buttonSaludar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSaludar.Location = new System.Drawing.Point(12, 338);
+            this.buttonSaludar.Name = "buttonSaludar";
+            this.buttonSaludar.Size = new System.Drawing.Size(88, 26);
+            this.buttonSaludar.TabIndex = 0;
+            this.buttonSaludar.Text = "Saludar a";
+            this.buttonSaludar.UseVisualStyleBackColor = true;
+            this.buttonSaludar.Click += new System.EventHandler(this.buttonSaludar_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(122, 187);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Correo";
+            // 
+            // textBoxCarnet
+            // 
+            this.textBoxCarnet.BackColor = System.Drawing.Color.Silver;
+            this.textBoxCarnet.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxCarnet.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxCarnet.Location = new System.Drawing.Point(185, 95);
+            this.textBoxCarnet.Name = "textBoxCarnet";
+            this.textBoxCarnet.Size = new System.Drawing.Size(121, 18);
+            this.textBoxCarnet.TabIndex = 1;
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.BackColor = System.Drawing.Color.Silver;
+            this.textBoxEmail.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxEmail.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxEmail.Location = new System.Drawing.Point(185, 184);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(319, 18);
+            this.textBoxEmail.TabIndex = 6;
+            // 
+            // textBoxSeccion
+            // 
+            this.textBoxSeccion.BackColor = System.Drawing.Color.Silver;
+            this.textBoxSeccion.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxSeccion.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSeccion.Location = new System.Drawing.Point(242, 157);
+            this.textBoxSeccion.Name = "textBoxSeccion";
+            this.textBoxSeccion.ReadOnly = true;
+            this.textBoxSeccion.Size = new System.Drawing.Size(32, 18);
+            this.textBoxSeccion.TabIndex = 7;
+            // 
+            // textBoxEstudiante
+            // 
+            this.textBoxEstudiante.BackColor = System.Drawing.Color.Silver;
+            this.textBoxEstudiante.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxEstudiante.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxEstudiante.Location = new System.Drawing.Point(185, 131);
+            this.textBoxEstudiante.Name = "textBoxEstudiante";
+            this.textBoxEstudiante.Size = new System.Drawing.Size(319, 18);
+            this.textBoxEstudiante.TabIndex = 8;
+            // 
+            // buttonCrear
+            // 
+            this.buttonCrear.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCrear.Location = new System.Drawing.Point(144, 257);
+            this.buttonCrear.Name = "buttonCrear";
+            this.buttonCrear.Size = new System.Drawing.Size(148, 58);
+            this.buttonCrear.TabIndex = 9;
+            this.buttonCrear.Text = "Crear registro";
+            this.buttonCrear.UseVisualStyleBackColor = true;
+            this.buttonCrear.Click += new System.EventHandler(this.buttonCrear_Click);
+            // 
+            // buttonBuscar
+            // 
+            this.buttonBuscar.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBuscar.Location = new System.Drawing.Point(316, 95);
+            this.buttonBuscar.Name = "buttonBuscar";
+            this.buttonBuscar.Size = new System.Drawing.Size(116, 31);
+            this.buttonBuscar.TabIndex = 10;
+            this.buttonBuscar.Text = "Buscar";
+            this.buttonBuscar.UseVisualStyleBackColor = true;
+            this.buttonBuscar.Click += new System.EventHandler(this.buttonBuscar_Click);
+            // 
+            // buttonActualizar
+            // 
+            this.buttonActualizar.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonActualizar.Location = new System.Drawing.Point(335, 257);
+            this.buttonActualizar.Name = "buttonActualizar";
+            this.buttonActualizar.Size = new System.Drawing.Size(148, 58);
+            this.buttonActualizar.TabIndex = 11;
+            this.buttonActualizar.Text = "Actualizar";
+            this.buttonActualizar.UseVisualStyleBackColor = true;
+            this.buttonActualizar.Click += new System.EventHandler(this.buttonActualizar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(130, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Carnet";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(122, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(122, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Sección";
+            // 
+            // buttonTareas
+            // 
+            this.buttonTareas.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonTareas.Location = new System.Drawing.Point(12, 12);
+            this.buttonTareas.Name = "buttonTareas";
+            this.buttonTareas.Size = new System.Drawing.Size(75, 29);
+            this.buttonTareas.TabIndex = 12;
+            this.buttonTareas.Text = "Tareas";
+            this.buttonTareas.UseVisualStyleBackColor = true;
+            this.buttonTareas.Click += new System.EventHandler(this.buttonTareas_Click);
+            // 
+            // comboBoxSeccion
+            // 
+            this.comboBoxSeccion.FormattingEnabled = true;
+            this.comboBoxSeccion.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C"});
+            this.comboBoxSeccion.Location = new System.Drawing.Point(185, 155);
+            this.comboBoxSeccion.Name = "comboBoxSeccion";
+            this.comboBoxSeccion.Size = new System.Drawing.Size(42, 21);
+            this.comboBoxSeccion.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 5.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(438, 106);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(139, 7);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Para buscar solo se requiere del carnet!!!!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.GhostWhite;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(635, 376);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBoxSeccion);
+            this.Controls.Add(this.buttonTareas);
+            this.Controls.Add(this.buttonActualizar);
+            this.Controls.Add(this.buttonBuscar);
+            this.Controls.Add(this.buttonCrear);
+            this.Controls.Add(this.textBoxEstudiante);
+            this.Controls.Add(this.textBoxSeccion);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.textBoxCarnet);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonSaludar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Crud alumnos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSaludar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxCarnet;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.TextBox textBoxSeccion;
+        private System.Windows.Forms.TextBox textBoxEstudiante;
+        private System.Windows.Forms.Button buttonCrear;
+        private System.Windows.Forms.Button buttonBuscar;
+        private System.Windows.Forms.Button buttonActualizar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonTareas;
+        private System.Windows.Forms.ComboBox comboBoxSeccion;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
